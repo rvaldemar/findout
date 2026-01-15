961173bc496717bde5e96f0f3f3a6b2eca10cb73
Rui Valdemar Santos <rvaldemar93@gmail.com>
2026-01-15 03:46:01 +0000

    Clean slate: Rails 8 base with authentication only, no business logic

--- app/controllers/brands_controller.rb
+++
@@ -1,7 +0,0 @@
-class BrandsController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-end
--- app/controllers/experiences_controller.rb
+++
@@ -1,7 +0,0 @@
-class ExperiencesController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-end
